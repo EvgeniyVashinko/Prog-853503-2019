f83770e8e1541e57c6b3f781e90223b7952edb45
EvgeniyVashinko <48275225+EvgeniyVashinko@users.noreply.github.com>
2019-06-25 18:42:46 +0300

    рп

---
+++ EvgeniyVashinko/1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+bool Сheck_lucky_num(int);
+int main()
+{
+	long long int num;
+	int counter = 0;
+	cin >> num;
+	for (long int i = 4; num > 3; i++) 
+	{
+		if (num % i == 0)
+		{
+			if (Сheck_lucky_num(i))
+			{
+				counter++;
+			}	
+			num /= i;
+			i--;
+		}
+		else { continue; }
+	}
+	cout << counter;
+	return 0;
+}
+
+bool Сheck_lucky_num(int num)
+{
+	while (num)
+	{
+		if (num % 10 == 4 || num % 10 == 7)
+		{
+			return true;
+		}
+		num /= 10;
+	}
+	return false;
+}
---
+++ EvgeniyVashinko/2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+int Minimal_element(int* arr, int N);
+void Conversion(int* arr, int min, int N);
+int Sum(int* arr, int N);
+int main()
+{
+	int N;
+	cout << "Enter the number of elements in array: ";
+	cin >> N;
+	cout << "Enter elements of array: ";
+	char str[1000];
+	cin.get();
+	gets_s(str);
+	char num[10] = " ";
+	int j = 0; // counter for curent num in string;
+	int k = 0; // number of elements in  array
+	int* A = new int[N];
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		num[j] = str[i];
+		j++;
+		if (str[i] == ' ' || str[i + 1] == '\0')
+		{
+			j = 0;
+			A[k] = atoi(num);
+			k++;
+			strcpy_s(num, "");
+		}
+		if (k > N) { break; }
+	}
+	int amount_of_conversation;
+	cout << "Enter amount of conversation: ";
+	cin >> amount_of_conversation;
+
+	for (int i = 0; i < amount_of_conversation; i++)
+	{
+		Conversion(A, Minimal_element(A, N), N);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << A[i] << " ";
+	}
+	cout << endl << Sum(A, N);
+}
+
+void Conversion(int* arr, int min, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = min - arr[i];
+	}
+}
+
+int Minimal_element(int* arr, int N)
+{
+	int min = arr[0];
+	for (int i = 1; i < N; i++)
+	{
+		if (min > arr[i]) { min = arr[i]; }
+	}
+	return min;
+}
+
+int Sum(int* arr, int N)
+{
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
